c497d60da8c3290acfdb9134a23a515bc9e274c2
Mikhail Lapshin <lapshin.michail@gmail.com>
2016-03-02 18:26:50 +0300

    watcher global module

--- watcher.js
+++ watcher.js
@@ -1,62 +1,59 @@
-var TOMCAT_HOST = 'localhost:8084';
-var CONTEXT     = '/sport';
-var LOGIN       = 'ide';
-var PASS        = 'Co8AAkc5';
-var DIRECTORIES = [
-    'app', 
-    'test'
-];
-var EXTENSIONS  = [
-    'js',
-    'txt'
-];
+#!/usr/bin/env node
 
+var fs = require('fs');
 var url = require('url');
 var http = require('http');
 var chokidar = require('chokidar');
 
-var changed = false;
+var filename = process.argv[2] ? process.argv[2] : "watcher-params.json";
 
-function checkChanges(){
-    if(changed){
-        changed = false;
-        reloadContext();
+fs.readFile(filename, (err, data) => {
+  if (err) throw err;
+  
+  var params = JSON.parse(data.toString());
+  var changed = false;
+  
+  function checkChanges(){
+        if(changed){
+            changed = false;
+            reloadContext();
+        }
     }
-}
 
-setInterval(checkChanges, 2000);
+    setInterval(checkChanges, 2000);
 
-function reloadContext(){
-    console.log("RELOADING CONTEXT: " + CONTEXT);
-    var URL = "http://" + LOGIN + ":" + PASS + "@" + TOMCAT_HOST + "/manager/text/reload?path=" + CONTEXT;
-    var req = http.request(url.parse(URL), (res) => {
-        res.setEncoding('utf8');
-        res.on('data', (chunk) => {
-            console.log(`BODY: ${chunk}`);
+    function reloadContext(){
+        console.log("RELOADING CONTEXT: " + params.CONTEXT);
+        var URL = "http://" + params.LOGIN + ":" + params.PASS + "@" + params.TOMCAT_HOST + "/manager/text/reload?path=" + params.CONTEXT;
+        var req = http.request(url.parse(URL), (res) => {
+            res.setEncoding('utf8');
+            res.on('data', (chunk) => {
+                console.log(`BODY: ${chunk}`);
+            });
+        });
+        req.on('error', (e) => {
+            console.log(`problem with request: ${e.message}`);
+        });
+        req.end();
+    }
+
+    var watcher = chokidar.watch('file, dir, glob, or array', {
+        ignored: /[\/\\]\./,
+        persistent: true
+    });
+
+    var watchPaths = [];
+
+    params.DIRECTORIES.forEach(function (dir) {
+        params.EXTENSIONS.forEach(function (ext) {
+            watchPaths.push(dir + '/**/*.' + ext);
         });
     });
-    req.on('error', (e) => {
-        console.log(`problem with request: ${e.message}`);
+
+    watcher.add(watchPaths);
+
+    watcher.on('all', (event, path) => {
+        console.log(event, path);
+        changed = true;
     });
-    req.end();
-}
-
-var watcher = chokidar.watch('file, dir, glob, or array', {
-  ignored: /[\/\\]\./,
-  persistent: true
-});
-
-var watchPaths = [];
-
-DIRECTORIES.forEach(function (dir) {
-    EXTENSIONS.forEach(function (ext) {
-        watchPaths.push(dir + '/**/*.' + ext);
-    });
-});
-
-watcher.add(watchPaths);
-
-watcher.on('all', (event, path) => {
-  console.log(event, path);
-  changed = true;
 });
