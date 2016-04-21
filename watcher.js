a3e50119093ddfbfb1579e862c3caaba4c026acc
Mikhail Lapshin <lapshin.michail@gmail.com>
2016-04-21 11:13:19 +0300

    add ingnoring js files with layout and fix ES6 compilier

--- watcher.js
+++ watcher.js
@@ -37,11 +37,15 @@ fs.readFile(filename, (err, data) => {
 	function ESCompile(){
 		ES6_FILES.forEach(function(file){
 			babel.transformFile(file, {}, function(err, res){
-				var outFile = file.split('.es6.').join('.');
-				fs.writeFile(outFile, res.code, function(err) {
-					if (err) throw err;
-					console.log("ES6 FILE COMPILIED: " + file + " -> " + outFile);
-				});
+				var outFile = file.split('.es6').join('.') + 'js';
+                if(res.code){
+                    fs.writeFile(outFile, res.code, function(err) {
+                        if (err) throw err;
+                        console.log("ES6 FILE COMPILIED: " + file + " -> " + outFile);
+                    });
+                } else {
+                    console.log("ERROR CONVERTING FILE FROM ES6");
+                }
 			});
 		});
 		ES6_FILES = [];
@@ -101,11 +105,21 @@ fs.readFile(filename, (err, data) => {
     watcher.on('all', (event, path) => {
         console.log(event, path);
         if(!firstRun){
-			if(event != 'unlink' && path.split('.es6.')[1])
-				ES6_FILES.push(path);
-			else
-				FILES.push(path);
-            changed = true;
+			if(event != 'unlink' && path.split('.').pop() == 'es6'){
+                console.log("ESS6");
+                ES6_FILES.push(path);
+                changed = true;
+            } else {
+                var layoutFile = path.split('.js').join('.') + 'layout';
+                fs.exists(layoutFile, (exists) => {
+                    if(!exists){
+                        FILES.push(path);
+                        changed = true;
+                    } else {
+                        console.log("LAYOUT FILE DETECTED, NO RESTART " + layoutFile);
+                    }
+                });      
+            }  
         } 
     });
 });
